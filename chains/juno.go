c6725b66387c2c95c2f58e5fdd1f1aed659b4082
Trevor Clarke <TrevorJTClarke@users.noreply.github.com>
2023-06-06 14:08:55 -0700

    Update juno.go to working RPC

--- chains/juno.go
+++ chains/juno.go
@@ -18,7 +18,7 @@ var JunoChain = junoChain{
 		bech32Prefix:      "juno",
 		denom:             "ujunox",
 		keyAlgo:           KeyAlgoSecp256k1,
-		rpc:               "https://rpc.uni.juno.deuslabs.fi:443",
+		rpc:               "https://uni-rpc.reece.sh:443",
 		grpc:              "http://juno-testnet-grpc.polkachu.com:12690",
 		nftImplementation: CosmWasm,
 	},
