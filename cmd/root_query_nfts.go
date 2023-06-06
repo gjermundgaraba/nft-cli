6cc40fd039d1e9def80320dea4e60ccaf43466d1
Gjermund Garaba <gjermund@garaba.net>
2023-06-06 15:56:45 +0200

    Added support for native cosmwasm nft transfer

--- cmd/root_query_nfts.go
+++ cmd/root_query_nfts.go
@@ -66,74 +66,51 @@ func getUsersNfts(ctx context.Context, clientCtx client.Context, chain chains.Ch
 
 		return chosenClass
 	case chains.CosmWasm:
-		classID := askForString("Class ID (full IBC path/trace)")
+		classID := askForString("Class ID (full IBC path/trace or contract address)")
 		splitClassID := strings.Split(classID, "/")
+
+		var nftContract string
+		var lastPort string
+		var lastChannel string
 		if len(splitClassID) <= 2 {
-			panic("only IBC path class IDs are supported for CosmWasm chains right now")
+			log.Println("Expecting Class ID to be a contract address...")
+			nftContract = classID
+		} else {
+			lastPort = splitClassID[0]
+			lastChannel = splitClassID[1]
+			bridgerContract := strings.TrimPrefix(lastPort, "wasm.")
+
+			nftContractQueryData, err := chains.Decoder.DecodeString(fmt.Sprintf(`{"nft_contract": {"class_id" : "%s"}}`, classID))
+			if err != nil {
+				panic(err)
+			}
+			wasmQueryClient := wasmdtypes.NewQueryClient(clientCtx)
+			queryContractByClassIDResponse, err := wasmQueryClient.SmartContractState(
+				ctx,
+				&wasmdtypes.QuerySmartContractStateRequest{
+					Address:   bridgerContract,
+					QueryData: nftContractQueryData,
+				},
+			)
+			if err != nil {
+				panic(err)
+			}
+			queryContractStringOutput, err := clientCtx.Codec.MarshalJSON(queryContractByClassIDResponse)
+			if err != nil {
+				panic(err)
+			}
+			var nftContractResponse queryNftContractResponse
+			if err := json.Unmarshal(queryContractStringOutput, &nftContractResponse); err != nil {
+				panic(err)
+			}
+			nftContract = nftContractResponse.Data
+			if nftContract == "" {
+				log.Fatal("no NFT contract found, make sure you are using the full IBC path/trace with port/channel/class-id")
+			}
 		}
 
-		lastPort := splitClassID[0]
-		lastChannel := splitClassID[1]
-		bridgerContract := strings.TrimPrefix(lastPort, "wasm.")
+		nfts := queryOwnedNFTSFromContract(ctx, clientCtx, address, nftContract)
 
-		nftContractQueryData, err := chains.Decoder.DecodeString(fmt.Sprintf(`{"nft_contract": {"class_id" : "%s"}}`, classID))
-		if err != nil {
-			panic(err)
-		}
-		wasmQueryClient := wasmdtypes.NewQueryClient(clientCtx)
-		queryContractByClassIDResponse, err := wasmQueryClient.SmartContractState(
-			ctx,
-			&wasmdtypes.QuerySmartContractStateRequest{
-				Address:   bridgerContract,
-				QueryData: nftContractQueryData,
-			},
-		)
-		if err != nil {
-			panic(err)
-		}
-		queryContractStringOutput, err := clientCtx.Codec.MarshalJSON(queryContractByClassIDResponse)
-		if err != nil {
-			panic(err)
-		}
-		var nftContractResponse queryNftContractResponse
-		if err := json.Unmarshal(queryContractStringOutput, &nftContractResponse); err != nil {
-			panic(err)
-		}
-		nftContract := nftContractResponse.Data
-		if nftContract == "" {
-			log.Fatal("no NFT contract found, make sure you are using the full IBC path/trace with port/channel/class-id")
-		}
-
-		nftsOwnedQueryData, err := chains.Decoder.DecodeString(fmt.Sprintf(`{"tokens":{"owner": "%s"}}`, address))
-		if err != nil {
-			panic(err)
-		}
-		queryOwnedNFTsResponse, err := wasmQueryClient.SmartContractState(
-			ctx,
-			&wasmdtypes.QuerySmartContractStateRequest{
-				Address:   nftContract,
-				QueryData: nftsOwnedQueryData,
-			},
-		)
-		if err != nil {
-			panic(err)
-		}
-		queryNFTSStringOutput, err := clientCtx.Codec.MarshalJSON(queryOwnedNFTsResponse)
-		if err != nil {
-			panic(err)
-		}
-		var nftsOwnedResponse queryNFTSOwnedResponse
-		if err := json.Unmarshal(queryNFTSStringOutput, &nftsOwnedResponse); err != nil {
-			panic(err)
-		}
-
-		var nfts []chains.NFT
-
-		for _, nft := range nftsOwnedResponse.Data.Tokens {
-			nfts = append(nfts, chains.NFT{
-				ID: nft,
-			})
-		}
 		return chains.NFTClass{
 			ClassID:         classID,
 			BaseClassID:     splitClassID[len(splitClassID)-1],
@@ -149,4 +126,40 @@ func getUsersNfts(ctx context.Context, clientCtx client.Context, chain chains.Ch
 	default:
 		panic("unknown NFT implementation")
 	}
+}
+
+func queryOwnedNFTSFromContract(ctx context.Context, clientCtx client.Context, address string, nftContract string) []chains.NFT {
+	wasmQueryClient := wasmdtypes.NewQueryClient(clientCtx)
+	nftsOwnedQueryData, err := chains.Decoder.DecodeString(fmt.Sprintf(`{"tokens":{"owner": "%s"}}`, address))
+	if err != nil {
+		panic(err)
+	}
+	queryOwnedNFTsResponse, err := wasmQueryClient.SmartContractState(
+		ctx,
+		&wasmdtypes.QuerySmartContractStateRequest{
+			Address:   nftContract,
+			QueryData: nftsOwnedQueryData,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	queryNFTSStringOutput, err := clientCtx.Codec.MarshalJSON(queryOwnedNFTsResponse)
+	if err != nil {
+		panic(err)
+	}
+	var nftsOwnedResponse queryNFTSOwnedResponse
+	if err := json.Unmarshal(queryNFTSStringOutput, &nftsOwnedResponse); err != nil {
+		panic(err)
+	}
+
+	var nfts []chains.NFT
+
+	for _, nft := range nftsOwnedResponse.Data.Tokens {
+		nfts = append(nfts, chains.NFT{
+			ID: nft,
+		})
+	}
+
+	return nfts
 }
